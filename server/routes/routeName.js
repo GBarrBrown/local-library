f5437fb0347c69aa3ae6f915c0a23f2e58a3115c
gbarrbrown <gbb7899@gmail.com>
2019-04-15 13:30:58 +1200

    Create example route

--- server/routes/routeName.js
+++ server/routes/routeName.js
@@ -1,4 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// GET /api/v1/routeName
+router.get('/', (req, res) => {
+
+})
+
+// GET /api/v1/routeName/:thing
+router.get('/:thing', (req, res) => {
+
+})
+
+// POST /api/v1/routeName
+router.post('/', (req, res) => {
+
+})
+
 module.exports = router
