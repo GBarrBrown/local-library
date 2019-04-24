ef81b269e619b8733d21c0ce3b9af39b03180744
gbarrbrown <gbb7899@gmail.com>
2019-04-25 06:42:44 +1200

    Create DB books migration

---
+++ migrations/20190425063528_books.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('books', table => {
+        table.increments('bookId').primary()// eg 7
+        table.string('title')               // eg Frankenstein's Monster
+        table.integer('authorId')           // eg 12
+        table.integer('gerneId')            // eg 3 - horror
+        table.integer('yearPublished')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('books')
+};
