28aef35a7249724c0edc0e2e8e8dbdbf928e3312
alex-h-k <kuan8@hotmail.com>
2019-04-28 14:35:56 +1200

    update db-functions-books

--- server/db/books.js
+++ server/db/books.js
@@ -1,11 +1,28 @@
-const connection = require('./connection')
+const connection = require("./connection");
 
 module.exports = {
-    getAllBooks
-}
+  getAllBooks,
+  getBookByTitle,
+  getBooksByAuthorFirstName
+};
 
 function getAllBooks(testDb) {
-    const db = testDb || connection
+  const db = testDb || connection;
 
-    return db('books')
+  return db("books");
+}
+
+function getBookByTitle(testDb, title) {
+  const db = testDb || connection;
+  return db("books")
+    .where("title", title)
+    .select();
+}
+
+function getBooksByAuthorFirstName(testDb, authorFirstName) {
+  const db = testDb || connection;
+  return db("authors")
+    .join("books", "authors.authorId", "books.authorId")
+    .where("authorFirstName", authorFirstName)
+    .select("books.title");
 }
